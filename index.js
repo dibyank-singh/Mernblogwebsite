1ac19922de38a1bb49d29c0c16a54ec8dade0fce
Dibyank <kumardivyank7@gmail.com>
2022-05-21 19:41:20 +0530

    done

--- index.js
+++ index.js
@@ -20,13 +20,22 @@ const __dirname= path.resolve()
 
 app.use( Router); 
 if(process.env.NODE_ENV ==="production"){ 
-    // app.use(express.static(path.join(__dirname ,"/client/build")))
-    app.get("*", (req, res) => {
-        let url = path.join(__dirname, '/client/build', 'index.html');
-        if (!url.startsWith('/app/')) // since we're on local windows
-          url = url.substring(1);
-        res.sendFile(url);
-      });
+    app.use(express.static(path.join(__dirname ,"/client/build")))
+  // List of all the files that should be served as-is
+  
+  let protect = ['transformed.js', 'main.css', 'favicon.ico']
+  app.get("*", (req, res) => {
+
+  let path = req.params['0'].substring(1)
+
+  if (protect.includes(path)) {
+    // Return the actual file
+    res.sendFile(`${__dirname}client/build/${path}`);
+  } else {
+    // Otherwise, redirect to /build/index.html
+    res.sendFile(`${__dirname}client/build/index.html`);
+  }
+});
     // app.get("*",(req,res)=>{
     //     res.sendFile(path.resolve(__dirname,"client","build","index.html"))
     // })
