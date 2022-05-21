1c4757c3853e65f1d3cdf9e2f0a11169f986e19f
Dibyank <kumardivyank7@gmail.com>
2022-05-21 19:22:04 +0530

    done

--- index.js
+++ index.js
@@ -21,9 +21,15 @@ const __dirname= path.resolve()
 app.use( Router); 
 if(process.env.NODE_ENV ==="production"){ 
     app.use(express.static(path.join(__dirname ,"/client/build")))
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,"client","build","index.html"))
-    })
+    app.get("*", (req, res) => {
+        let url = path.join(__dirname, '/client/build', 'index.html');
+        if (!url.startsWith('/app/')) // since we're on local windows
+          url = url.substring(1);
+        res.sendFile(url);
+      });
+    // app.get("*",(req,res)=>{
+    //     res.sendFile(path.resolve(__dirname,"client","build","index.html"))
+    // })
 } else {
     app.get("/",(req , res)=>{
         res.send("api server is running test ")
